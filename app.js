fb482b83d8a7c62e08624d585ff41b5075269989
Brooke Weil <brooke.marie.weil@gmail.com>
2016-12-10 18:37:23 -0500

    Orgazined app.js and did getColor

--- app.js
+++ app.js
@@ -1,57 +1,54 @@
+var ctx;
+
 window.onload = function(){ 
 
+    // Initialize buttons and canvas
+    initButtons();
 
-document.getElementById("buttonRed").onclick = function(){
-        document.getElementById("currcolor").style = "background-color: red";
+    var canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+    ctx.moveTo(0,0);
+
+    var color;
+
+    var xprediction;
+    var yprediction;
+
+    webgazer.setGazeListener(function(data, elapsedTime) {
+        if (data == null) {
+                return;
+        }
+        getCurrentColor();
+        var xprediction = data.x; //these x coordinates are relative to the viewport 
+        var yprediction = data.y; //these y coordinates are relative to the viewport
+        ctx.lineTo(xprediction,yprediction);
+        ctx.stroke();
+        console.log(elapsedTime); //elapsed time is based on time since begin was called
+            }).begin();
 };
-document.getElementById("buttonOrange").onclick = function(){
-        document.getElementById("currcolor").style = "background-color: orange";
-};
-document.getElementById("buttonYellow").onclick = function(){
-        document.getElementById("currcolor").style = "background-color: yellow";
-};
-document.getElementById("buttonGreen").onclick = function(){
-        document.getElementById("currcolor").style = "background-color: green";
-};
-document.getElementById("buttonBlue").onclick = function(){
-        document.getElementById("currcolor").style = "background-color: blue";
-};
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();
+function initButtons () {
+    document.getElementById("buttonRed").onclick = function(){
+            document.getElementById("currcolor").style = "background-color: red";
+    };
+    document.getElementById("buttonOrange").onclick = function(){
+            document.getElementById("currcolor").style = "background-color: orange";
+    };
+    document.getElementById("buttonYellow").onclick = function(){
+            document.getElementById("currcolor").style = "background-color: yellow";
+    };
+    document.getElementById("buttonGreen").onclick = function(){
+            document.getElementById("currcolor").style = "background-color: green";
+    };
+    document.getElementById("buttonBlue").onclick = function(){
+            document.getElementById("currcolor").style = "background-color: blue";
+    };
+}
 
-var xprediction;
-var yprediction;
+function getCurrentColor() {
+    console.log ("Color is: ")
+    console.log (jQuery('#currcolor').css("background-color"));
+    ctx.strokeStyle=(jQuery('#currcolor').css("background-color"));
+    // console.log (currcolor.style.backgroundColor)
+}
 
-webgazer.setGazeListener(function(data, elapsedTime) {
-    if (data == null) {
-        return;
-    }
-    xprediction = data.x; //these x coordinates are relative to the viewport 
-    yprediction = data.y; //these y coordinates are relative to the viewport
-    console.log("Time elapsed: " + elapsedTime ); //elapsed time is based on time since begin was called
-    console.log("X Coord " + xprediction ); 
-    console.log("Y Coord " + yprediction ); 
-}).begin();
-
-
-
-ctx.lineTo(600,100);
-ctx.stroke();
-
-webgazer.setGazeListener(function(data, elapsedTime) {
-                if (data == null) {
-                        return;
-                }
-                var xprediction = data.x; //these x coordinates are relative to the viewport 
-                var yprediction = data.y; //these y coordinates are relative to the viewport
-                ctx.lineTo(xprediction,yprediction);
-                ctx.stroke();
-                console.log(elapsedTime); //elapsed time is based on time since begin was called
-        }).begin();
-
-
-};
