a306a14b5c41cceaee0898b7b09f0f6bcb43ed8a
Maya DeBellis <mayadebellis@gmail.com>
2016-12-10 17:38:45 -0500

    gfsnlg

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-document.getElementById("button1").onclick = function(){
-        document.getElementById("currcolor") = "red";
-}
+window.onload = function(){ 
 
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-var img = document.getElementById("scream");
-ctx.drawImage(img,10,10);
+
+document.getElementById("buttonRed").onclick = function(){
+        document.getElementById("currcolor").style = "background-color: red";
+};
+document.getElementById("buttonOrange").onclick = function(){
+        document.getElementById("currcolor").style = "background-color: orange";
+};
+document.getElementById("buttonYellow").onclick = function(){
+        document.getElementById("currcolor").style = "background-color: yellow";
+};
+document.getElementById("buttonGreen").onclick = function(){
+        document.getElementById("currcolor").style = "background-color: green";
+};
+document.getElementById("buttonBlue").onclick = function(){
+        document.getElementById("currcolor").style = "background-color: blue";
+};
+
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
+// var img = document.getElementById("scream");
+// ctx.drawImage(img,10,10);
+
+console.log("yo");
+
+};
